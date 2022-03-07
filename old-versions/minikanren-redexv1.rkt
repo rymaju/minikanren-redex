aa180fce29c02f6fd3ef32938433ed459a19004d
Ryan Jung <ryan.matthew.jung@gmail.com>
2022-03-06 22:09:27 -0500

    Create minikanren-redexv1.rkt

---
+++ old-versions/minikanren-redexv1.rkt
@@ -0,0 +1,169 @@
+#lang racket
+(require redex)
+(require redex/reduction-semantics)
+
+;                                                                                  
+;                                                                                  
+;                                                                                  
+;    ;;                                                                            
+;    ;;                                  ;                             ;           
+;    ;;        ;;;;;    ;; ;;;;     ;;;;;;  ;;    ;    ;;;;;      ;;;;;;    ;;;;   
+;    ;;        ;  ;;;   ;;;; ;;    ;; ;;    ;;    ;    ;  ;;;    ;; ;;     ;;; ;;  
+;    ;;            ;;   ;;    ;   ;;   ;;   ;;    ;        ;;   ;;   ;;   ;;    ;  
+;    ;;        ;;;;;;   ;;    ;   ;;   ;;   ;;    ;    ;;;;;;   ;;   ;;   ;;    ;; 
+;    ;;       ;;;  ;;   ;;    ;   ;;;  ;;   ;;    ;   ;;;  ;;   ;;;  ;;   ;;;;;;;; 
+;    ;;       ;;   ;;   ;;    ;    ;;;;;    ;;    ;   ;;   ;;    ;;;;;    ;;       
+;    ;;       ;;   ;;   ;;    ;    ;        ;;    ;   ;;   ;;    ;        ;;       
+;    ;;;;;;;  ;;; ;;;   ;;    ;    ;;        ;;  ;;   ;;; ;;;    ;;        ;;; ;;  
+;    ;;;;;;;   ;;;; ;;  ;;    ;    ;;;;;;    ;;;; ;    ;;;; ;;   ;;;;;;     ;;;;   
+;                                       ;;                            ;;           
+;                                 ;;    ;;                      ;;    ;;           
+;                                 ;;;  ;;;                      ;;;  ;;;           
+;                                  ;;;;;                         ;;;;;             
+;                                                                                  
+;                                                                                  
+
+
+; e ::= ≐ | s | ans v e
+; p ::= (Γ e)
+; minikanren wants to capture the search order
+
+(define-language L+e
+  [e ::=
+     ·
+     s
+     ((T σ) e)])
+; make sure v subset of e
+
+(define-language L
+  ;------------------------------------
+  ; Expressions
+ 
+  [Γ (d ...)]  ; Relation Environments
+
+  ; (Γ g) -> (Γ g2) -> (Γ g3) ??
+  
+  ; ideally we want a:
+  ; search tree -> search tree
+  ; maybe add s to v
+  ; if we instead of using g, had an s?
+  
+  
+  [p ::=
+     (Γ e)]      ; Programs
+  [d ((r x) g)]  ; Relations
+  
+  ; Goals
+  [g T           ; Trivial success
+     ⊥           ; Trivial failure
+     (t =? t)    ; Syntactic equality
+     (g V g)     ; Disjunction
+     (g ∧ g)     ; Conjuction
+     (r t)       ; Relation call
+     ((∃ x)  g)] ; Variable introduction
+
+  ;-------------------------------------
+  ; Values and Search Trees
+
+  ; Values
+  [v ·             ; Empty Node
+     ((T σ) ∨ v)] ; Answer Disjunct (yuck the letter v and logical or look the same
+
+
+  ;((g ∨ g) σ) => ((g σ) ∨ (g σ))
+  ; copy the number metafunction thingy for logic vars
+  ; state is gross though?
+  ; maybe σ = (((x y)... counter)
+  ; Search Trees
+  [s ·
+     (g σ)
+     (s ∨ s)
+     (delay s)]
+
+  [e ::=
+     ·
+     s
+     ((T σ) ∨ e)]
+
+  
+  ; NOTE: changed from d -> delay because d is already the name for the relation nonterminal
+  ;-------------------------------------
+  ; Evaluation Contexts
+  
+  ; Answer Stream
+  [Ev hole 
+       ((T σ) ∨ Ev)]
+  
+  ; Search Tree
+  [Es hole
+       (Es ∨ s)]
+  
+  ; Goal
+  [Eg hole
+       (Eg ∧ g)]
+
+
+  ;Terms
+  [t c
+     x
+     (t t...)]
+
+  ;Other
+  [r variable-not-otherwise-mentioned] ; to account for arbitrary relation names
+  [x variable-not-otherwise-mentioned] ; to account for arbitrary parameter names
+  [c number
+     symbol
+     boolean
+     string]
+  [σ ((x y) ...)]
+  )
+
+
+;                                                                                            
+;                            ;                                     ;;                        
+;    ;;;;;                    ;                                    ;;                        
+;    ;   ;;                   ;                         ;;                                   
+;    ;    ;;                  ;                         ;;                                   
+;    ;    ;;    ;;;;      ;;; ;    ;    ;      ;;;;   ;;;;;;;    ;;;;       ;;;;     ; ;;;   
+;    ;    ;;   ;;  ;;    ;;  ;;    ;    ;    ;;   ;     ;;          ;      ;;  ;;    ;;  ;;  
+;    ;    ;    ;    ;   ;;    ;    ;    ;    ;          ;;          ;      ;    ;    ;    ;  
+;    ;;;;;    ;;    ;;  ;;    ;    ;    ;    ;          ;;          ;     ;;    ;;   ;    ;  
+;    ;  ;     ;;;;;;;;  ;;    ;    ;    ;   ;;          ;;          ;     ;;    ;;   ;    ;  
+;    ;  ;;    ;;        ;;    ;    ;    ;    ;          ;;          ;     ;;    ;;   ;    ;  
+;    ;   ;;    ;        ;;    ;    ;    ;    ;          ;;          ;     ;;    ;    ;    ;  
+;    ;    ;    ;;   ;    ;;  ;;    ;   ;;    ;;   ;      ;  ;       ;      ;;  ;;    ;    ;  
+;    ;    ;;    ;;;;      ;;; ;    ;;;; ;     ;;;;;      ;;;;;   ;;;;;;     ;;;;     ;    ;  
+;                                                                                            
+;                                                                                            
+;                                                                                            
+;                                                                                            
+;                                                                                            
+;                                                                                            
+
+
+(define red
+  (reduction-relation
+   L
+   #:domain p
+   [--> (in-hole Ev (delay s))  ; Ev[d s] => Ev[s] ; Delay Rule
+        (in-hole Ev s)
+        ]))
+; Starting off with an easy rule that doesn't need metafunctions
+
+;(redex-match L g (term (delay (T σ))))
+
+;(redex-match L p (term [· (delay (T σ))]))
+;                       ^ "·" is not a Γ (line 30)? hmm
+; Will fail
+(test--> 
+   red
+   (term [() (delay (T σ))]) ; This isn't really in the shape of a "program" p so it fails
+   (term [() (T σ)]))
+
+; Q1: Should we have σ in the language somewhere?
+; Q2: What are some examples of p? Seems like I'm getting confused on that part :/
+;(redex-match L r (term caro))
+(test-results)
+
+(redex-match L d (term ((caro x) T)))
+(redex-match L t (term 10))  
