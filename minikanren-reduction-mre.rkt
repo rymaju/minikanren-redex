74660994d8f4866ffcd56e6b7f1e896555aefdae
Jason Hemann <jason.hemann@gmail.com>
2022-05-06 06:51:32 -0400

    MInimizing MRE

--- minikanren-reduction-mre.rkt
+++ minikanren-reduction-mre.rkt
@@ -2,16 +2,74 @@
 (require redex/gui)
 (require redex/reduction-semantics)
 
-(define-language L [g ⊤ p (◇ g)])
+;; Here is a little language.
+
+(define-language L [f ⊤ (◇ f)])
+
+;; We have only one reduction rule, to removes the diamond modality
 
 (define red
   (reduction-relation L
-    #:domain g
-    [--> (◇ g) g "Manifest"]
-    [--> p ⊤ "triviality"]))
+    #:domain f
+    [--> (◇ f) f "Manifest"]))
 
-(pretty-print "we successively use this reduction relation, each time
-producing the one and only value.")
+;; Below is a term we expect to produce.
+
+(term ⊤)
+
+;; Here is the failing test where we claim we produce it.
+
+(test--> 
+ red
+ (term (◇ (◇ ⊤)))
+ (term ⊤))
+
+;; However, traces says we should succeed. Watch.
+
+(traces
+ red
+ (term (◇ (◇ ⊤))))
+
+;; Curiously, though, the first goes to the second, and
+;; the second goes to the third.
+
+(test--> 
+ red
+ (term (◇ (◇ ⊤)))
+ (term (◇ ⊤)))
+
+(test--> 
+ red
+ (term (◇ ⊤))
+ (term ⊤))
+
+;; It seems to go wrong at the diamond modality reduction---a
+;; consequence of "fresh subst" or how and where we apply it. If we
+;; add extra layers, it always fails, returning instead the result of
+;; the first reduction.
+
+(test-->
+ red
+ (term (◇ (◇ (◇ ⊤))))
+ (term ⊤))
+
+;; Even though, again, traces works just fine. 
+
+(traces
+ red
+ (term (◇ (◇ (◇ ⊤)))))
+
+;; Deceptively, it *looks* like we get an extra set of parens, and
+;; like that would explain the failure. But I don't think that's the
+;; real problem. 
+
+(apply-reduction-relation*
+ red
+ (term (◇ ⊤)))
+
+;; I think somehow that we're getting those extra parens because it's
+;; giving back a list of answers, maybe
+
 (first
  (apply-reduction-relation
   red
@@ -21,57 +79,12 @@ producing the one and only value.")
     (first
      (apply-reduction-relation
       red
-      (first
-       (apply-reduction-relation
-        red
-        (term (◇ (◇ (◇ p))))))))))))
+      (term (◇ (◇ (◇ ⊤))))))))))
 
-(pretty-print "Below is the term we expect to produce.")
-(term ⊤)
+;; We successively use this reduction relation, each time
+;; producing the one and only value.
 
-(pretty-print "Here is the failing test where we claim we produce it.")
-(test--> 
- red
- (term (◇ p))
- (term ⊤))
+;; Is this a Redex bug? It seems too superficial for me to be the
+;; first one to notice it.
 
-(pretty-print "However, traces says we should succeed. Watch.")
-(traces
- red
- (term (◇ p)))
-
-(pretty-print "Curiously, though, the first goes to the second, and
-the second goes to the third.")
-(test--> 
- red
- (term (◇ p))
- (term p))
-
-(test--> 
- red
- (term p)
- (term ⊤))
-
-(pretty-print "This seems to go wrong at the existential reduction---a
-consequence of \"fresh subst\" or how and where we apply it. If we add
-extra layers, it always fails, returning instead the result of the
-first reduction.")
-(test-->
- red
- (term (◇ (◇ (◇ p))))
- (term ⊤))
-
-(traces
- red
- (term (◇ (◇ (◇ p)))))
-
-(pretty-print "Deceptively, it *looks* like we get an extra set of
-parens, and like that would explain the failure. But I don't think
-that's the real problem.")
-(apply-reduction-relation*
- red
- (term (◇ p)))
-
-(pretty-print "I think somehow that we're getting those extra parens
-because it's giving back a list of answers, maybe")
 
