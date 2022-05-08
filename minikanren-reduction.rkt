e8224cb842228694bf49a1901708391dafa00da2
Jason Hemann <jason.hemann@gmail.com>
2022-05-08 18:45:07 -0400

    Fixing unify and tests for that

--- minikanren-reduction.rkt
+++ minikanren-reduction.rkt
@@ -3,7 +3,7 @@
 (require redex/reduction-semantics)
 (require rackunit)
 (check-redundancy #t)
-(current-traced-metafunctions 'all)
+;; (current-traced-metafunctions 'all)
 
 ;; Consider, if we separate answer streams from search tree
 ;; disjuncts, then we would need some rule to "move into the
@@ -88,65 +88,83 @@
 
 (module+ test
 
-;; FAILS
-;; "bald symbols are terms"
-;; (redex-match? L t (term cat))
+  ;; FAILS
+  ;; "bald symbols are terms"
+  ;; (redex-match? L t (term cat))
 
-"bald numbers are terms"
-(redex-match? L t (term 5))
+  "bald numbers are terms"
+  (redex-match? L t (term 5))
 
-"booleans are terms"
-(redex-match? L t (term #t))
+  "booleans are terms"
+  (redex-match? L t (term #t))
 
-"strings are terms"
-(redex-match? L t (term "cat"))
+  "strings are terms"
+  (redex-match? L t (term "cat"))
 
-"a fresh call over an equation is a goal"
-(redex-match? L g (term (∃ x:x (x:x =? "seven"))))
+  "a fresh call over an equation is a goal"
+  (redex-match? L g (term (∃ x:x (x:x =? "seven"))))
 
-(redex-match? L sub (term ()))
+  (redex-match? L sub (term ()))
 
-(redex-match? L σ (term (state () 0)))
+  (redex-match? L σ (term (state () 0)))
 
-"A goal w/a state is a search tree"
-(redex-match? L s (term ((∃ x:x (x:x =? "seven")) (state () 0))))
+  "A goal w/a state is a search tree"
+  (redex-match? L s (term ((∃ x:x (x:x =? "seven")) (state () 0))))
 
-"A goal w/a state is a query expression"
-(redex-match? L e (term ((∃ x:x (x:x =? "seven")) (state () 0))))
+  "A goal w/a state is a query expression"
+  (redex-match? L e (term ((∃ x:x (x:x =? "seven")) (state () 0))))
 
-(redex-match? L
- p
- (term 
-  (prog ((r:add x:x (x:x =? "cat"))) ())))
+  (redex-match?
+   L
+   p
+   (term 
+    (prog ((r:add x:x (x:x =? "cat"))) ())))
 
-"matching a small unify program with symbol constants"
-(redex-match? L p (term (prog () (("cat" =? "cat") (state () 0)))))
+  "matching a small unify program with symbol constants"
+  (redex-match? L p (term (prog () (("cat" =? "cat") (state () 0)))))
 
-"matching a small unify program with symbol constants and non-empty subst"
-(redex-match? L 
- p              
- (term (prog () (("seven" =? "seven") (state ((2 "fish")) 0)))))
+  "matching a small unify program with symbol constants and non-empty subst"
+  (redex-match?
+   L 
+   p              
+   (term (prog () (("seven" =? "seven") (state ((2 "fish")) 0)))))
 
-"matching a program w a relation"
-(redex-match?
- L
- p
- (term 
-  (prog ((add x:x (x:x =? "cat"))) (⊤ (state () 0)))))
+  "matching a program w a relation"
+  (redex-match?
+   L
+   p
+   (term 
+    (prog ((r:add x:x (x:x =? "cat"))) (⊤ (state () 0)))))
 
-"matching a full program with a relation call"
-(redex-match?
- L
- p
- (term 
-  (prog ((r:add x:x (x:x =? "cat"))) ((r:add "dog") (state () 0)))))
+  "matching a full program with a relation call"
+  (redex-match?
+   L
+   p
+   (term 
+    (prog ((r:add x:x (x:x =? "cat"))) ((r:add "dog") (state () 0)))))
 
-"Small relation lookup matches reduction pattern"
-(redex-match? L 
- (prog ((r_0 x_0 g_0) ... (r_1 x_1 g_1) (r_2 x_2 g_2) ...) (in-hole Ev (in-hole Es ((r_1 t) σ))))
- (term (prog ((r:foo x:x ("seven" =? "seven"))) ((r:foo "seven") (state () 0)))))
+  "Small relation lookup matches reduction pattern"
+  (redex-match?
+   L 
+   (prog ((r_0 x_0 g_0) ... (r_1 x_1 g_1) (r_2 x_2 g_2) ...) (in-hole Ev (in-hole Es ((r_1 t) σ))))
+   (term (prog ((r:foo x:x ("seven" =? "seven"))) ((r:foo "seven") (state () 0)))))
 
-)
+  (redex-match?
+   L
+   p
+   (term (prog ((r:add x:x (∃ x:a
+                              (∃ x:d
+                                 ((x:x =? (x:a : x:d))
+                                  ∧ (((x:a =? "z")
+                                      ∧ (x:d =? ("s" : "z")))
+                                     ∨ (∃ x:a2
+                                          (∃ x:d2
+                                             (((x:a : x:d) =? (("s" : x:a2) : ("s" : x:d2)))
+                                              ∧ (r:add (x:a2 : x:d2)))))))))))
+               ((∃ x:y (r:add (("s" : ("s" : ("s" : "z"))) : x:y))) (state () 0)))))
+
+
+  )
 
 (define-metafunction L
   unify : t t sub -> maybe-sub
@@ -154,8 +172,8 @@
   [(unify natural t sub) (ext natural t sub)]
   [(unify t natural sub) (ext natural t sub)]
   [(unify (t_1a : t_1b) (t_2a : t_2b) sub)
-   (unify t_1b t_2b sub_1)
-   (where sub_1 (unify t_1a t_2a sub))]
+   (unify (walk t_1b sub_1) (walk t_2b sub_1) sub_1)
+   (where sub_1 (unify (walk t_1a sub) (walk t_2a sub) sub))]
   [(unify t_1 t_1 sub) sub]
   [(unify _ _ _) #f])
 
@@ -178,28 +196,37 @@
 
 
 (module+ test
+
+  (test-equal
+   (term (unify "seven" "seven" ((2 "fish"))))
+   (term ((2 "fish"))))
   
-(test-equal
- (term (unify 0 0 ()))
- (term ()))
+  (test-equal
+   (term (unify 0 0 ()))
+   (term ()))
 
-(test-equal
- (term (unify 0 "cat" ()))
- (term ((0 "cat"))))
+  (test-equal
+   (term (unify 0 "cat" ()))
+   (term ((0 "cat"))))
 
-(test-equal
- (term (unify "cat" 0 ()))
- (term ((0 "cat"))))
+  (test-equal
+   (term (unify "cat" 0 ()))
+   (term ((0 "cat"))))
+  
+  (test-equal
+   (term (walk 0 ((1 "cat") (0 "dog"))))
+   (term "dog"))
 
-(test-equal
- (term (walk 0 ((1 "cat") (0 "dog"))))
- (term "dog"))
+  (test-equal
+   (term (walk 0 ((1 "cat") (0 1))))
+   (term "cat"))
 
-(test-equal
- (term (walk 0 ((1 "cat") (0 1))))
- (term "cat"))
-
-)
+  (test-equal
+   (term
+    (unify (walk (1 : 2) ((0 2) (1 "z")))
+           (walk (("s" : 3) : ("s" : 4)) ((0 2) (1 "z")))
+           ((0 2) (1 "z"))))
+    (term #f)))
 
 (define red 
   (reduction-relation L
@@ -247,7 +274,7 @@
 
     [--> (in-hole EΓ (in-hole Ev (in-hole Es ((t_1 =? t_2) (state sub c)))))
          (in-hole EΓ (in-hole Ev (in-hole Es (⊤ (state (unify (walk t_1 sub) (walk t_2 sub) sub) c)))))
-         (where ((x t) ...) (unify (walk t_1 sub) (walk t_2 sub) sub))
+         (where ((natural t) ...) (unify (walk t_1 sub) (walk t_2 sub) sub))
          "unify succeed"]
 
     [--> (in-hole EΓ (in-hole Ev (in-hole Es ((t_1 =? t_2) (state sub c)))))
@@ -274,58 +301,45 @@
          (in-hole EΓ (in-hole Ev ()))
          "prune bald failure"]))
 
+(module+ test 
+  (test-->>
+   red
+   (term (prog () (("seven" =? "seven") (state () 0))))
+   (term (prog () (⊤ (state () 0)))))
 
-(test-->>
- red
- (term (prog () (("seven" =? "seven") (state () 0))))
- (term (prog () (⊤ (state () 0)))))
+  (test-->>
+   red
+   (term (prog () ((∃ x:x ⊤) (state () 0))))
+   (term (prog () (⊤ (state () 1)))))
 
-(test-->>
- red
- (term (prog () ((∃ x:x ⊤) (state () 0))))
- (term (prog () (⊤ (state () 1)))))
+  (test-->>
+   red
+   (term (prog ((r:foo x:x ("seven" =? "seven"))) ((r:foo "seven") (state () 0))))
+   (term (prog ((r:foo x:x ("seven" =? "seven"))) (⊤ (state () 0)))))
 
-(apply-reduction-relation* red (term (prog ((r:foo x:x ("seven" =? "seven"))) ((r:foo "seven") (state () 0)))))
+  (test-->> 
+   red
+   #:equiv alpha-equivalent?
+   (term (prog ((r:foo x:x ("seven" =? "seven"))) ((r:foo "seven") (state () 0))))
+   (term (prog ((r:foo x:x ("seven" =? "seven"))) (⊤ (state () 0)))))
 
-(test-->> 
- red
- #:equiv alpha-equivalent?
- (term (prog ((r:foo x:x ("seven" =? "seven"))) ((r:foo "seven") (state () 0))))
- (term (prog ((r:foo x:x ("seven" =? "seven"))) (⊤ (state () 0)))))
-
-;; (traces
-;;  red
-;;  (term (prog ((r:foo x:x ("seven" =? "seven"))) ((r:foo "seven") (state () 0)))))
-
-;; This is a state mid-run 
-;; (traces
-;;  red
-;;  (term (prog () ((∃ x:x ("seven" =? "seven")) (⊤ (state () 0)))))
-
-;; This is a state mid-run 
-;; (traces
-;;  red
-;;  (term (prog () ((∃ x:x ("seven" =? "seven")) (⊤ (state () 0))))))
-
-
-(test-->>
- red
- #:equiv alpha-equivalent?
- (term (prog () (("seven" =? "seven") (state ((2 "fish")) 0))))
- (term (prog () (⊤ (state ((2 "fish")) 0)))))
+  (test-->>
+   red
+   #:equiv alpha-equivalent?
+   (term (prog () (("seven" =? "seven") (state ((2 "fish")) 0))))
+   (term (prog () (⊤ (state ((2 "fish")) 0)))))
 
   ;; Tests substitution doesn't subst constants 
 
 
-(test-->>
- red
- #:equiv alpha-equivalent?
- (term 
-  (prog ((r:add x:x (x:x =? "cat"))) ((r:add "dog") (state () 0))))
- (term 
-  (prog ((r:add x:x (x:x =? "cat"))) ())))
+  (test-->>
+   red
+   #:equiv alpha-equivalent?
+   (term 
+    (prog ((r:add x:x (x:x =? "cat"))) ((r:add "dog") (state () 0))))
+   (term 
+    (prog ((r:add x:x (x:x =? "cat"))) ())))
 
-(module+ test
   (test-->>
    red
    #:equiv alpha-equivalent?
@@ -374,7 +388,6 @@
    red
    (term (prog () ((⊤ (state ((3 "fish")) 0)) + (⊥ #f))))
    (term (prog () ((⊤ (state ((3 "fish")) 0)) + ()))))
-
 
   (test-->>
    red
@@ -444,43 +457,41 @@
                       ((3 "fish"))
                       0))))))))))))
 
+  (test-->>
+   red
+   (term 
+    (prog ((r:add x:x (∃ x:a
+                         (∃ x:d
+                            ((x:x =? (x:a : x:d))
+                             ∧ (((x:a =? "z")
+                                 ∧ (x:d =? ("s" : "z")))
+                                ∨ (∃ x:a2
+                                     (∃ x:d2
+                                        (((x:a : x:d) =? (("s" : x:a2) : ("s" : x:d2)))
+                                         ∧ (r:add (x:a2 : x:d2)))))))))))
+          ((∃ x:y (x:y =? x:y))
+           (state () 0))))
+   (term (prog ((r:add x:x (∃ x:a
+                              (∃ x:d
+                                 ((x:x =? (x:a : x:d))
+                                  ∧ (((x:a =? "z")
+                                      ∧ (x:d =? ("s" : "z")))
+                                     ∨ (∃ x:a2
+                                          (∃ x:d2
+                                             (((x:a : x:d) =? (("s" : x:a2) : ("s" : x:d2)))
+                                              ∧ (r:add (x:a2 : x:d2)))))))))))
+               (⊤ (state () 1)))))
+
 
   (test-->>
    red
-   #:equiv alpha-equivalent?
    (term 
-    (prog ((add x (∃ a
-                     (∃ d
-                        ((x =? (a : d))
-                         ∧ (((a =? z)
-                             ∧ (d =? (s : z)))
-                            ∨ (∃ a2
-                                 (∃ d2
-                                    (((a : d) =? ((s : a2) : (s : d2)))
-                                     ∧ (add (a2 : d2)))))))))))
-          ((∃ y (y =? y))
+    (prog ((r:add x:x (∃ x:a (x:a =? x:x))))
+          ((∃ x:y (r:add (("s" : "z") : x:y)))
            (state () 0))))
-   (term (prog ((add x (∃ a
-                          (∃ d
-                             ((x =? (a : d))
-                              ∧ (((a =? z)
-                                  ∧ (d =? (s : z)))
-                                 ∨ (∃ a2
-                                      (∃ d2
-                                         (((a : d) =? ((s : a2) : (s : d2)))
-                                          ∧ (add (a2 : d2)))))))))))
-               (state () 0))))
-
-
-  (test-->>
-   red
-   #:equiv alpha-equivalent?
-   (term 
-    (prog ((add x (∃ a (a =? x))))
-          ((∃ y (add ((s : z) : y)))
-           (state () 0))))
-   (term (prog ((add x (∃ a (a =? x))))
-               (state () 0))))
+   (term
+    (prog ((r:add x:x (∃ x:a (x:a =? x:x))))
+          (⊤ (state ((1 (("s" : "z") : 0))) 2)))))
 
   ;; Questionable test right now 
   (test-->>
@@ -488,77 +499,132 @@
    #:equiv alpha-equivalent?
    (term 
     (prog 
-     ((add x (∃ a
-                (∃ d
-                   (x =? (a : d))))))
-     ((∃ y (add ((s : (s : (s : z))) : y)))
+     ((r:add x:x (∃ x:a
+                    (∃ x:d
+                       (x:x =? (x:a : x:d))))))
+     ((∃ x:y (r:add (("s" : ("s" : ("s" : "z"))) : x:y)))
       (state () 0))))
    (term (prog
-          ((add x (∃ a (∃ d (x =? (a : d))))))
-          (state ((d y) (a (s : (s : (s : z))))) 3))))
+          ((r:add x:x (∃ x:a (∃ x:d (x:x =? (x:a : x:d))))))
+          ((⊤ (state ((2 ("s" : "z")) (0 2) (1 "z")) 3)) + ()))))
 
-  (redex-match? L p
-                (term (prog ((add x (∃ a
-                                       (∃ d
-                                          ((x =? (a : d))
-                                           ∧ (((a =? z)
-                                               ∧ (d =? (s : z)))
-                                              ∨ (∃ a2
-                                                   (∃ d2
-                                                      (((a : d) =? ((s : a2) : (s : d2)))
-                                                       ∧ (add (a2 : d2)))))))))))
-                            ((∃ y (add ((s : (s : (s : z))) : y))) (state () 0)))))
-
+  ;; This test seems too slow or not working. Test.
   (test-->>
    red
    (term
     (prog
-     ((add x
-           (∃ a
-              (∃ d ((x =? (a : d))
-                    ∧
-                    (((a =? z)
-                      ∧ (d =? (s : z)))
-                     ∨ (∃ a2
-                          (∃ d2
-                             (((a : d) =? ((s : a2) : (s : d2)))
-                              ∧ (add (a2 : d2)))))))))))
-     ((∃ y (add ((s : (s : (s : z))) : y))) (state () 0))))
-   (term (prog () ())))
-
-  ;; (traces red (term (((((⊤
-  ;;                        ∧ ("seven" =? "seven"))
-  ;;                       ∨ (("eight" =? "eight")
-  ;;                          ∧ ("nine" =? "nine")))
-  ;;                      ∧ ((⊥
-  ;;                          ∨ ("seven" =? "seven"))
-  ;;                         ∨ (("eight" =? "eight")
-  ;;                            ∧ ("nine" =? "nine"))))
-  ;;                     ∨ (((⊥
-  ;;                          ∨ ("seven" =? "seven"))
-  ;;                         ∨ (("eight" =? "eight")
-  ;;                            ∧ ("nine" =? "nine")))
-  ;;                        ∧ ((⊤
-  ;;                            ∧ ("seven" =? "seven"))
-  ;;                           ∨ (("eight" =? "eight")
-  ;;                              ∧ ("nine" =? "nine")))))
-  ;;                    (state ((x 3)) 0))))
-
-  ;; (traces red (term
-  ;;              ((((⊤ (state ((x 3)) 0))
-  ;;                 ∨
-  ;;                 (("eight" =? "eight")
-  ;;                  (state ((x 3)) 0)))
-  ;;                ∧
-  ;;                (("seven" =? "seven")
-  ;;                 ∨
-  ;;                 ("nine" =? "nine")))
-  ;;               ∨
-  ;;               (("seventeen" =? "seventeen")
-  ;;                (state ((x 3)) 0)))))
+     ((r:add x:x
+             (∃ x:a
+                (∃ x:d ((x:x =? (x:a : x:d))
+                        ∧
+                        (((x:a =? "z")
+                          ∧ (x:d =? ("s" : "z")))
+                         ∨ (∃ x:a2
+                              (∃ x:d2
+                                 (((x:a : x:d) =? (("s" : x:a2) : ("s" : x:d2)))
+                                  ∧ (r:add (x:a2 : x:d2)))))))))))
+     ((∃ x:y (r:add (("s" : ("s" : ("s" : "z"))) : x:y))) (state () 0))))
+   (term
+    (prog
+     ((r:add x:x
+             (∃ x:a
+                (∃ x:d ((x:x =? (x:a : x:d))
+                        ∧
+                        (((x:a =? "z")
+                          ∧ (x:d =? ("s" : "z")))
+                         ∨ (∃ x:a2
+                              (∃ x:d2
+                                 (((x:a : x:d) =? (("s" : x:a2) : ("s" : x:d2)))
+                                  ∧ (r:add (x:a2 : x:d2)))))))))))
+     ((⊤
+      (state
+       ((14 ("s" : "z"))
+        (12 14)
+        (13 "z")
+        (10 ("s" : 12))
+        (11 "z")
+        (8 10)
+        (9 ("s" : "z"))
+        (6 ("s" : 8))
+        (7 ("s" : "z"))
+        (4 6)
+        (5 ("s" : ("s" : "z")))
+        (2 ("s" : 4))
+        (3 ("s" : ("s" : "z")))
+        (0 2)
+        (1 ("s" : ("s" : ("s" : "z")))))
+       15))
+     +
+     ()))))
   
   )
 
+
+#;
+(module+ traces
+  (traces
+   red
+   (term (prog ((r:foo x:x ("seven" =? "seven"))) ((r:foo "seven") (state () 0)))))
+
+  ;; This is a state mid-run 
+  (traces
+   red
+   (term (prog () ((∃ x:x ("seven" =? "seven")) (⊤ (state () 0))))))
+
+  ;; This is a state mid-run 
+  (traces
+   red
+   (term (prog () ((∃ x:x ("seven" =? "seven")) (⊤ (state () 0))))))
+
+  
+
+(traces
+ red
+ (term
+  (prog
+   ((r:add x:x
+           (∃ x:a
+              (∃ x:d ((x:x =? (x:a : x:d))
+                      ∧
+                      (((x:a =? "z")
+                        ∧ (x:d =? ("s" : "z")))
+                       ∨ (∃ x:a2
+                            (∃ x:d2
+                               (((x:a : x:d) =? (("s" : x:a2) : ("s" : x:d2)))
+                                ∧ (r:add (x:a2 : x:d2)))))))))))
+   ((∃ x:y (r:add ("z" : x:y))) (state () 0)))))
+
+
+  (traces red (term (((((⊤
+                         ∧ ("seven" =? "seven"))
+                        ∨ (("eight" =? "eight")
+                           ∧ ("nine" =? "nine")))
+                       ∧ ((⊥
+                           ∨ ("seven" =? "seven"))
+                          ∨ (("eight" =? "eight")
+                             ∧ ("nine" =? "nine"))))
+                      ∨ (((⊥
+                           ∨ ("seven" =? "seven"))
+                          ∨ (("eight" =? "eight")
+                             ∧ ("nine" =? "nine")))
+                         ∧ ((⊤
+                             ∧ ("seven" =? "seven"))
+                            ∨ (("eight" =? "eight")
+                               ∧ ("nine" =? "nine")))))
+                     (state ((3 "x")) 0))))
+
+  (traces red (term
+               ((((⊤ (state ((3 "x")) 0))
+                  ∨
+                  (("eight" =? "eight")
+                   (state ((3 "x")) 0)))
+                 ∧
+                 (("seven" =? "seven")
+                  ∨
+                  ("nine" =? "nine")))
+                ∨
+                (("seventeen" =? "seventeen")
+                 (state ((3 "x")) 0))))))
 
 (test-results)
  
